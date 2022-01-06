be186b3d3f353768bfd6d6055c2f4662d37b5a4e
donghyunKim <imkim1893@naver.com>
2022-01-06 06:42:36 +0000

    first init

---
+++ _code/lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:unbound_size_test_flutter/page/MainPage.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MainPage(),
+    );
+  }
+}
